60fdc85f85eb53009819e3dd4ec031361ebd1a21
Martin Kováčik <martn.kovacik@gmail.com>
2022-01-23 12:37:00 +0100

    exercises 3.1-3.8

--- index.js
+++ index.js
@@ -1,8 +1,104 @@
-const http = require('http')
+const express = require('express')
+const app = express()
 
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' })
-  response.end('Hello World')
+const morgan = require('morgan')
+
+morgan.token('body', function getPostBody (req) {
+
+    return JSON.stringify(req.body)
+})
+
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(express.json())
+
+let persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/api/persons', (request, response) => {
+
+    response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if(person){
+
+        response.json(person)
+    }
+    else{
+
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+
+    const id = Number(request.params.id)
+    
+    if(persons.find(person => person.id === id)){
+        persons = persons.filter(person => person.id !== id)
+        response.status(204).end()
+    }
+    else{
+
+        response.status(404).end()
+    }
+})
+
+app.post('/api/persons', (request, response) => {
+
+    const body = request.body
+
+    if(!body.name || !body.number){
+
+        return response.status(400).json({error: 'name or number is missing'})
+    }
+
+    if(persons.find(person => person.name === body.name)){
+
+        return response.status(409).json({error: 'name already exists'})
+    }
+
+    const idMin = Math.ceil(5)
+    const idMax = Math.floor(10000)
+
+    const randId = Math.floor(Math.random() * (idMax - idMin) + idMin)
+
+    const person = request.body
+    person.id = randId
+
+    persons = persons.concat(person)
+
+    response.json(person)
+})
+
+app.get('/info', (request, response) => {
+
+    response.send(`Phonebook has info for ${persons.length} people.<br><br> ${new Date()}`)
 })
 
 const PORT = 3001
